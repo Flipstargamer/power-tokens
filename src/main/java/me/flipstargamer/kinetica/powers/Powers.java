6d55ab1dda5c0676ab0bb25f406ab91f4e8fc515
Flipstargamer <107770254+Flipstargamer@users.noreply.github.com>
2026-01-07 15:53:21 +0200

    Removed Deprecated functions from Powers and added helper method for registry.

--- src/main/java/me/flipstargamer/kinetica/powers/Powers.java
+++ src/main/java/me/flipstargamer/kinetica/powers/Powers.java
@@ -9,6 +9,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.neoforged.bus.api.IEventBus;
@@ -24,69 +25,28 @@ import java.util.Objects;
 public class Powers {
     public static final DeferredRegister<Power> POWERS = DeferredRegister.create(KineticaRegistries.POWER_REGISTRY, Kinetica.MOD_ID);
 
-    public static final DeferredHolder<Power, @NotNull AttributePower> HEALTH_POWER = POWERS.register("more_health",
-            () -> new AttributePower(Attributes.MAX_HEALTH, 10, AttributeModifier.Operation.ADD_VALUE,
-                    Identifier.parse("kinetica:more_health_power")));
+    public static final DeferredHolder<Power, @NotNull AttributePower> HEALTH_POWER =
+            registerAttribute("more_health", Attributes.MAX_HEALTH, 10, AttributeModifier.Operation.ADD_VALUE);
 
-    public static final DeferredHolder<Power, @NotNull AttributePower> SPEED_POWER = POWERS.register("more_speed",
-            () -> new AttributePower(Attributes.MOVEMENT_SPEED, 0.6f, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL,
-                    Identifier.parse("kinetica:more_speed_power")));
+    public static final DeferredHolder<Power, @NotNull AttributePower> SPEED_POWER =
+            registerAttribute("natural_armor", Attributes.MOVEMENT_SPEED, 0.6f, AttributeModifier.Operation.ADD_MULTIPLIED_BASE);
 
-    public static final DeferredHolder<Power, @NotNull AttributePower> TINY_POWER = POWERS.register("tiny",
-            () -> new AttributePower(Attributes.SCALE, -0.2f, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL,
-                    Identifier.parse("kinetica:tiny_power")));
+    public static final DeferredHolder<Power, @NotNull AttributePower> TINY_POWER =
+            registerAttribute("natural_armor", Attributes.ARMOR, -0.5f, AttributeModifier.Operation.ADD_MULTIPLIED_BASE);
 
-    public static final DeferredHolder<Power, @NotNull AttributePower> NATURAL_ARMOR = POWERS.register("natural_armor",
-            () -> new AttributePower(Attributes.ARMOR, 4, AttributeModifier.Operation.ADD_VALUE,
-                    Identifier.parse("kinetica:armor_power")));
+    public static final DeferredHolder<Power, @NotNull AttributePower> NATURAL_ARMOR =
+            registerAttribute("natural_armor", Attributes.ARMOR, 5, AttributeModifier.Operation.ADD_VALUE);
+
+    private static DeferredHolder<Power, @NotNull AttributePower> registerAttribute(String name,
+                                                                                   Holder<Attribute> attributeHolder,
+                                                                                   float value,
+                                                                                   AttributeModifier.Operation operation) {
+        return POWERS.register(name,
+                () -> new AttributePower(attributeHolder, value, operation,
+                        Identifier.fromNamespaceAndPath(Kinetica.MOD_ID, name + "_power")));
+    }
 
     public static void register(IEventBus bus) {
         POWERS.register(bus);
-    }
-
-    @Deprecated(since = "1.0.0", forRemoval = true)
-    public static void addPower(LivingEntity entity, Holder<Power> power) {
-        List<Holder<Power>> powers = entity.getData(ModDataAttachments.PLAYER_POWERS);
-
-        if (powers.contains(power))
-            return;
-
-        powers.add(power);
-
-        Power truePower = power.value();
-        truePower.apply(entity);
-
-        if (entity instanceof ServerPlayer player) {
-            ModTriggerTypes.POWER_OBTAINED_TRIGGER.get().trigger(player, power.value());
-        }
-    }
-
-    @Deprecated(since = "1.0.0", forRemoval = true)
-    public static void addPower(LivingEntity entity, Power power) {
-        addPower(entity, KineticaRegistries.POWER_REGISTRY.wrapAsHolder(power));
-    }
-
-    @Deprecated(since = "1.0.0", forRemoval = true)
-    public static void removePower(LivingEntity entity, Holder<Power> power) {
-        List<Holder<Power>> powers = entity.getData(ModDataAttachments.PLAYER_POWERS);
-
-        if (!powers.contains(power))
-            return;
-
-        powers.remove(power);
-
-        Power truePower = power.value();
-        truePower.remove(entity);
-    }
-
-    @Deprecated(since = "1.0.0", forRemoval = true)
-    public static void removePower(LivingEntity entity, Power power) {
-        removePower(entity, KineticaRegistries.POWER_REGISTRY.wrapAsHolder(power));
-    }
-
-    @Deprecated(since = "1.0.0", forRemoval = true)
-    public static Component getPowerTranslation(Holder<Power> power) {
-        Identifier key = Objects.requireNonNull(power.getKey()).identifier();
-        return Component.translatable("powers." + key.getNamespace() + "." + key.getPath());
     }
 }
