183f991292e1d61af8b061ee3f9a18bdfbc7ec87
Flipstargamer <107770254+Flipstargamer@users.noreply.github.com>
2026-01-12 12:20:41 +0200

    Advancement Updates - PowerObtainedTriggerInstance replaced by PowerObtainedTrigger.Instance. - All Powers Advancement - Vampire Advancement

--- src/main/java/me/flipstargamer/powertokens/advancements/PowerObtainedTriggerInstance.java
+++ src/main/java/me/flipstargamer/powertokens/advancements/PowerObtainedTriggerInstance.java
@@ -4,23 +4,18 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import me.flipstargamer.powertokens.powers.power.Power;
 import me.flipstargamer.powertokens.powers.PowerPredicate;
-import net.minecraft.advancements.Criterion;
 import net.minecraft.advancements.criterion.ContextAwarePredicate;
 import net.minecraft.advancements.criterion.EntityPredicate;
 import net.minecraft.advancements.criterion.SimpleCriterionTrigger;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
+@Deprecated(since = "1.0.0", forRemoval = true)
 public record PowerObtainedTriggerInstance(Optional<ContextAwarePredicate> player, PowerPredicate predicate) implements SimpleCriterionTrigger.SimpleInstance {
     public static final Codec<PowerObtainedTriggerInstance> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             EntityPredicate.ADVANCEMENT_CODEC.optionalFieldOf("player").forGetter(PowerObtainedTriggerInstance::player),
             PowerPredicate.CODEC.fieldOf("power").forGetter(PowerObtainedTriggerInstance::predicate)
     ).apply(instance, PowerObtainedTriggerInstance::new));
-
-    public static Criterion<@NotNull PowerObtainedTriggerInstance> create(PowerPredicate predicate) {
-        return ModTriggerTypes.POWER_OBTAINED_TRIGGER.value().createCriterion(new PowerObtainedTriggerInstance(Optional.empty(), predicate));
-    }
 
     public boolean matches(Power power) {
         return predicate.test(power);
--- src/main/java/me/flipstargamer/powertokens/datagen/ModAdvancementProvider.java
+++ src/main/java/me/flipstargamer/powertokens/datagen/ModAdvancementProvider.java
@@ -1,29 +1,34 @@
 package me.flipstargamer.powertokens.datagen;
 
+import me.flipstargamer.powertokens.PowerTokenRegistries;
 import me.flipstargamer.powertokens.PowerTokens;
-import me.flipstargamer.powertokens.advancements.PowerObtainedTriggerInstance;
+import me.flipstargamer.powertokens.advancements.PowerObtainedTrigger;
 import me.flipstargamer.powertokens.advancements.PowersWipedTrigger;
 import me.flipstargamer.powertokens.items.ModItems;
 import me.flipstargamer.powertokens.powers.PowerPredicate;
 import me.flipstargamer.powertokens.powers.Powers;
+import me.flipstargamer.powertokens.powers.power.Power;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.AdvancementType;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.advancements.AdvancementSubProvider;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
 
 public class ModAdvancementProvider implements AdvancementSubProvider {
     @Override
     public void generate(@NotNull HolderLookup.Provider registries, @NotNull Consumer<AdvancementHolder> writer) {
+        List<Holder<Power>> allPowers = PowerTokenRegistries.POWER_REGISTRY.stream().map(PowerTokenRegistries.POWER_REGISTRY::wrapAsHolder).toList();
+
         AdvancementHolder root = Advancement.Builder.advancement()
                 .display(
                         ModItems.POWER_TOKEN.get(),
@@ -35,10 +40,10 @@ public class ModAdvancementProvider implements AdvancementSubProvider {
                         true,
                         false
                 )
-                .addCriterion("obtain_a_power", PowerObtainedTriggerInstance.create(new PowerPredicate(Optional.empty())))
+                .addCriterion("obtain_a_power", PowerObtainedTrigger.createCriterion(new PowerPredicate(Optional.empty())))
                 .save(writer, easyId("main/root"));
 
-        Advancement.Builder.advancement()
+        AdvancementHolder speedAdvancement = Advancement.Builder.advancement()
                 .display(
                         Items.RABBIT_FOOT,
                         Component.translatable("advancements.power_tokens.speed.title"),
@@ -50,11 +55,11 @@ public class ModAdvancementProvider implements AdvancementSubProvider {
                         false
                 )
                 .addCriterion("obtain_speed_boost_power",
-                        PowerObtainedTriggerInstance.create(new PowerPredicate(Optional.of(Powers.SPEED_POWER))))
+                        PowerObtainedTrigger.createCriterion(new PowerPredicate(Optional.of(Powers.SPEED_POWER))))
                 .parent(root)
                 .save(writer, easyId("main/speed_boost"));
 
-        Advancement.Builder.advancement()
+        AdvancementHolder wipedPowers = Advancement.Builder.advancement()
                 .display(
                         ModItems.INVERTED_TOKEN.get(),
                         Component.translatable("advancements.power_tokens.wiped.title"),
@@ -68,6 +73,42 @@ public class ModAdvancementProvider implements AdvancementSubProvider {
                 .addCriterion("wiped_powers", PowersWipedTrigger.createCriterion())
                 .parent(root)
                 .save(writer, easyId("main/wiped_powers"));
+
+        Advancement.Builder allPowersBuilder = Advancement.Builder.advancement()
+                .display(
+                        ModItems.REINFORCED_TOKEN.get(),
+                        Component.translatable("advancements.power_tokens.all_powers.title"),
+                        Component.translatable("advancements.power_tokens.all_powers.description"),
+                        null,
+                        AdvancementType.CHALLENGE,
+                        true,
+                        true,
+                        true
+                )
+                .parent(wipedPowers);
+
+        for (Holder<Power> power : allPowers) {
+            allPowersBuilder.addCriterion("has_" + Objects.requireNonNull(power.getKey()).identifier().getPath(),
+                    PowerObtainedTrigger.createCriterion(new PowerPredicate(Optional.of(power))));
+        }
+
+        allPowersBuilder.save(writer, easyId("main/all_powers"));
+
+        AdvancementHolder vampireAdvancement = Advancement.Builder.advancement()
+                .display(
+                        Items.GUNPOWDER,
+                        Component.translatable("advancements.power_tokens.vampire.title"),
+                        Component.translatable("advancements.power_tokens.vampire.description"),
+                        null,
+                        AdvancementType.TASK,
+                        true,
+                        true,
+                        false
+                )
+                .parent(speedAdvancement)
+                .addCriterion("has_vampire",
+                        PowerObtainedTrigger.createCriterion(new PowerPredicate(Optional.of(Powers.VAMPIRE))))
+                .save(writer, easyId("main/vampire"));
     }
 
     private Identifier easyId(String path) {
