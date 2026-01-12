5e9fe2217889cf75898297888c0f571476685b56
Flipstargamer <107770254+Flipstargamer@users.noreply.github.com>
2026-01-12 12:20:41 +0200

    Advancement Updates - PowerObtainedTriggerInstance replaced by PowerObtainedTrigger.Instance. - All Powers Advancement - Vampire Advancement

--- src/main/java/me/flipstargamer/powertokens/advancements/PowerObtainedTrigger.java
+++ src/main/java/me/flipstargamer/powertokens/advancements/PowerObtainedTrigger.java
@@ -1,18 +1,40 @@
 package me.flipstargamer.powertokens.advancements;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import me.flipstargamer.powertokens.powers.PowerPredicate;
 import me.flipstargamer.powertokens.powers.power.Power;
+import net.minecraft.advancements.Criterion;
+import net.minecraft.advancements.criterion.ContextAwarePredicate;
+import net.minecraft.advancements.criterion.EntityPredicate;
 import net.minecraft.advancements.criterion.SimpleCriterionTrigger;
 import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.NotNull;
 
-public class PowerObtainedTrigger extends SimpleCriterionTrigger<@NotNull PowerObtainedTriggerInstance> {
+import java.util.Optional;
+
+public class PowerObtainedTrigger extends SimpleCriterionTrigger<PowerObtainedTrigger.@NotNull Instance> {
     @Override
-    public @NotNull Codec<PowerObtainedTriggerInstance> codec() {
-        return PowerObtainedTriggerInstance.CODEC;
+    public @NotNull Codec<Instance> codec() {
+        return Instance.CODEC;
     }
 
     public void trigger(ServerPlayer player, Power power) {
         this.trigger(player, triggerInstance -> triggerInstance.matches(power));
+    }
+
+    public static Criterion<@NotNull Instance> createCriterion(PowerPredicate predicate) {
+        return ModTriggerTypes.POWER_OBTAINED_TRIGGER.value().createCriterion(new Instance(Optional.empty(), predicate));
+    }
+
+    public record Instance(Optional<ContextAwarePredicate> player, PowerPredicate predicate) implements SimpleCriterionTrigger.SimpleInstance {
+        public static final Codec<Instance> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+                EntityPredicate.ADVANCEMENT_CODEC.optionalFieldOf("player").forGetter(Instance::player),
+                PowerPredicate.CODEC.fieldOf("power").forGetter(Instance::predicate)
+        ).apply(instance, Instance::new));
+
+        public boolean matches(Power power) {
+            return predicate.test(power);
+        }
     }
 }
